2f03ce914c468e5fbf5e0a0b9ebecc83d9fc4158
1000zoo <cjswldn99@gmail.com>
2022-09-14 21:36:50 +0900

    programmers / level1 / 연습문제 / 짝수와 홀수

---
+++ src/codingtest_practice/level1/OddEven.java
@@ -0,0 +1,10 @@
+//짝수와 홀수
+//https://school.programmers.co.kr/learn/courses/30/lessons/12937
+
+package codingtest_practice.level1;
+
+public class OddEven {
+    public String solution(int num) {
+        return num % 2 == 0 ? "Even" : "Odd";
+    }
+}
