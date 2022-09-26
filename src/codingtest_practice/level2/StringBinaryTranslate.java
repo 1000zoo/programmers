5465c0a4fbb29f65e24568c6d932164876ad5c4c
1000zoo <cjswldn99@gmail.com>
2022-09-26 21:28:58 +0900

    programmers / level2 / 월간 코드 챌린지 시즌 1 / 이진 변환 반복하기

---
+++ src/codingtest_practice/level2/StringBinaryTranslate.java
@@ -0,0 +1,35 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/70129
+//2022-09-26
+//이진 변환 반복하기
+
+package codingtest_practice.level2;
+
+public class StringBinaryTranslate {
+    public static int[] solution(String s) {
+        int[] answer = new int[2];
+        String temp;
+        while (!s.equals("1")) {
+            temp = s;
+            s = s.replace("0", "");
+            answer[1] += temp.length() - s.length();
+            answer[0]++;
+            s = toBinary(s.length());
+        }
+
+        return answer;
+    }
+
+    private static String toBinary(int n) {
+        StringBuilder result = new StringBuilder();
+        while (n != 0) {
+            result.append(n % 2);
+            n /= 2;
+        }
+        return result.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "01110";
+        solution(s);
+    }
+}
