58a28ae46f5a2372b49cfbcdea4993c3d9113685
1000zoo <cjswldn99@gmail.com>
2022-10-23 17:52:41 +0900

    programmers / level1 / 2019 Kakao Blind Recruitment / 실패율

---
+++ src/codingtest_practice/level1/FailRate.java
@@ -0,0 +1,31 @@
+package codingtest_practice.level1;
+
+import java.util.*;
+
+public class FailRate {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = {};
+        SortedMap<Double, List<Integer>> map = new TreeMap<>(Collections.reverseOrder());
+        for (int i = 1; i <= N; i++) {
+            int clear = 0;
+            int fail = 0;
+            for (int stage : stages) {
+                if (stage > i) clear++;
+                else if (stage == i) fail++;
+            }
+            double failRate = 0;
+            if (fail != 0) {
+                failRate = (double) fail / (clear + fail);
+            }
+            List<Integer> temp = map.getOrDefault(failRate, new ArrayList<>());
+            temp.add(i);
+            map.put(failRate, temp);
+
+        }
+        List<Integer> list = new ArrayList<>();
+        for (double key : map.keySet()) {
+            list.addAll(map.get(key));
+        }
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
