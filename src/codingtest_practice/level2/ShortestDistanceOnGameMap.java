46398c0c708d47b99273f009dc560ad34f4b9975
1000zoo <cjswldn99@gmail.com>
2022-12-29 17:17:17 +0900

    programmers / level2 / dfs & bfs / 게임 맵 최단 거리

--- src/codingtest_practice/level2/ShortestDistanceOnGameMap.java
+++ src/codingtest_practice/level2/ShortestDistanceOnGameMap.java
@@ -1,66 +1,52 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/1844#
+//2022-12-29
+//게임 맵 최단거리
+
 package codingtest_practice.level2;
 
 import java.util.*;
 
-public class ShortestDistanceOnGameMap {
-
+class ShortestDistanceOnGameMap {
     private int[][] map;
-    private Set<String> visited = new HashSet<>();
-    private boolean arrive = false;
 
+    //참조 : https://school.programmers.co.kr/questions/38232
     public int solution(int[][] maps) {
         map = maps;
-        int answer = dfs(new int[] {0, 0}, Integer.MAX_VALUE, 1);
-        return arrive ? answer : -1;
-    }
+        boolean[][] visitied = new boolean[maps.length][maps[0].length];
+        Queue<int[]> nodes = new LinkedList<>();
+        int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        nodes.add(new int[]{0, 0});
 
-    private int dfs(int[] curr, int min, int count) {
-        Stack<int[]> actions = getAction(curr);
-        if (actions.isEmpty()) {
-            if (curr[0] == map[0].length - 1 && curr[1] == map.length - 1) {
-                arrive = true;
-                return Math.min(count, min);
+        while (!nodes.isEmpty()) {
+            int[] curr = nodes.remove();
+            int x = curr[0];
+            int y = curr[1];
+
+            if (x == map[0].length - 1 && y == map.length - 1) {
+                return map[y][x];
             }
-            return min;
-        }
 
-        while (!actions.isEmpty()) {
-            int[] next = actions.pop();
-            visited.add(encode(next));
-            min = Math.min(dfs(next, min, count + 1), min);
-            visited.remove(encode(next));
-        }
-        return min;
-    }
+            for (int[] d : dir) {
+                int tempX = curr[0] + d[0];
+                int tempY = curr[1] + d[1];
+                if (!canMove(tempX, tempY) || !isRoad(tempX, tempY)) continue;
 
-    public Stack<int[]> getAction(int[] curr) {
-        Stack<int[]> actions = new Stack<>();
-        int[][] directions = new int[][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-        for (int[] dir : directions) {
-            int[] next = new int[] {curr[0] + dir[0], curr[1] + dir[1]};
-            if (checkBoundary(next)) {
-                if (map[next[1]][next[0]] == 1 && !visited.contains(encode(next))) {
-                    actions.push(next);
+                if (isRoad(tempX, tempY) && !visitied[tempY][tempX]) {
+                    visitied[y][x] = true;
+                    nodes.add(new int[]{tempX, tempY});
+                    map[tempY][tempX] = map[y][x] + 1;
                 }
             }
         }
-        return actions;
+
+        return -1;
     }
 
-    private String encode(int x, int y) {
-        return x + "/" + y;
-    }
-    private String encode(int[] coor) {
-        return coor[0] + "/" + coor[1];
+    private boolean canMove(int x, int y) {
+        return x >= 0 && x < map[0].length && y >= 0 && y < map.length;
     }
 
-    private int[] decode(String code) {
-        String[] temp = code.split("/");
-        return new int[] {Integer.parseInt(temp[0]), Integer.parseInt(temp[1])};
-    }
-
-    private boolean checkBoundary(int[] next) {
-        return next[0] >= 0 && next[0] < map[0].length && next[1] >= 0 && next[1] < map.length;
+    private boolean isRoad(int x, int y) {
+        return map[y][x] != 0;
     }
 }
