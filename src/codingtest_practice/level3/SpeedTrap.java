e2c4c26ff5ad4b97be9d3edc9ca24992f2e01e41
1000zoo <cjswldn99@gmail.com>
2023-01-23 22:52:28 +0900

    programmers / level3 / Greedy / 단속카메라

--- src/codingtest_practice/level3/SpeedTrap.java
+++ src/codingtest_practice/level3/SpeedTrap.java
@@ -7,15 +7,15 @@ package codingtest_practice.level3;
 import java.util.*;
 
 public class SpeedTrap {
+
+    //참조 : https://school.programmers.co.kr/questions/42373
     public int solution(int[][] routes) {
         int answer = 0;
 
-        Arrays.sort(routes, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[0] != o2[0]) return o1[0] - o2[0];
-                return o1[1] - o2[1];
-            }
+        //참조 : https://ifuwanna.tistory.com/328 (람다로 다시 수정)
+        Arrays.sort(routes, (o1, o2) -> {
+            if (o1[0] != o2[0]) return o1[0] - o2[0];
+            return o1[1] - o2[1];
         });
 
         int prev = Integer.MIN_VALUE;
