15e3e3f02e55b461053a772fd258c252bd4d3993
1000zoo <cjswldn99@gmail.com>
2023-02-08 14:34:03 +0900

    programmers / level1 / 연습문제 / 둘만의 암호

---
+++ src/codingtest_practice/level1/SecretCodeOfUs.java
@@ -0,0 +1,41 @@
+//2023-02-07
+//https://school.programmers.co.kr/learn/courses/30/lessons/155652
+//둘만의 암호
+
+package codingtest_practice.level1;
+
+import java.util.*;
+
+public class SecretCodeOfUs {
+
+    //런타임 에러
+    public String solution(String s, String skip, int index) {
+        StringBuilder answer = new StringBuilder();
+        Map<Character, Integer> alpList = new HashMap<>();
+        Map<Integer, Character> indList = new HashMap<>();
+        Set<Character> skipSet = new HashSet<>();
+
+        for (char c : skip.toCharArray()) {
+            skipSet.add(c);
+        }
+
+        int i = 0;
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (!skipSet.contains(c)) {
+                alpList.put(c, i);
+                indList.put(i++, c);
+            }
+        }
+
+        for (char c : s.toCharArray()) {
+            int temp = alpList.get(c) + index;
+            int alpIndex = checkBoundary(temp, alpList.size());
+            answer.append((char) (indList.get(alpIndex)));
+        }
+        return answer.toString();
+    }
+
+    private int checkBoundary(int index, int length) {
+        return index < length ? index : index - length;
+    }
+}
