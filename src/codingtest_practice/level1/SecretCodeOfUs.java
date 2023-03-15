f13aa8f7fe73a06942c72dc18f576475dc16104f
1000zoo <cjswldn99@gmail.com>
2023-03-15 21:03:47 +0900

    programmers / level1 / 둘만의 암호 / 간결한 풀이로 수정

--- src/codingtest_practice/level1/SecretCodeOfUs.java
+++ src/codingtest_practice/level1/SecretCodeOfUs.java
@@ -7,37 +7,24 @@ package codingtest_practice.level1;
 import java.util.*;
 
 public class SecretCodeOfUs {
-
-    //런타임 에러
     public String solution(String s, String skip, int index) {
         StringBuilder answer = new StringBuilder();
-        Map<Character, Integer> alpList = new HashMap<>();
-        Map<Integer, Character> indList = new HashMap<>();
-        Set<Character> skipSet = new HashSet<>();
+        List<Character> list = new ArrayList<>();
 
-        for (char c : skip.toCharArray()) {
-            skipSet.add(c);
-        }
-
-        int i = 0;
         for (char c = 'a'; c <= 'z'; c++) {
-            if (!skipSet.contains(c)) {
-                alpList.put(c, i);
-                indList.put(i++, c);
+            if (!skip.contains("" + c)) {
+                list.add(c);
             }
         }
 
         for (char c : s.toCharArray()) {
-            int temp = alpList.get(c) + index;
-            int alpIndex = checkBoundary(temp, alpList.size());
-            answer.append((char) (indList.get(alpIndex)));
+            int temp = list.indexOf(c) + index;
+            int alpIndex = checkBoundary(temp, list.size());
+            answer.append((char) list.get(alpIndex));
         }
         return answer.toString();
     }
 
-    //index - length 로 할 경우,
-    //index - length > length 일 때 null 관련 에러발생
-    //=> index % length
     private int checkBoundary(int index, int length) {
         return index < length ? index : index % length;
     }
