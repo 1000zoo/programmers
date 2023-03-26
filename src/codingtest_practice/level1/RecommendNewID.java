0ba008c61eb884adac56d544da4c716c72a41525
1000zoo <cjswldn99@gmail.com>
2023-03-27 01:48:41 +0900

    programmers / level1 / 2021 KAKAO BLIND RECRUITMENT / 신규 아이디 추천

---
+++ src/codingtest_practice/level1/RecommendNewID.java
@@ -0,0 +1,82 @@
+package codingtest_practice.level1;
+
+public class RecommendNewID {
+    public String solution(String new_id) {
+        new_id = step1(new_id);
+        new_id = step2(new_id);
+        new_id = step3(new_id);
+        new_id = step4(new_id);
+        new_id = step5(new_id);
+        new_id = step6(new_id);
+        new_id = step7(new_id);
+        return new_id;
+    }
+
+    private String step1(String new_id) {
+        return new_id.toLowerCase();
+    }
+
+    private String step2(String new_id) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : new_id.toCharArray()) {
+            if (isValidChar(c)) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    private String step3(String new_id) {
+        boolean dot = false;
+        StringBuilder sb = new StringBuilder();
+        for (char c : new_id.toCharArray()) {
+            if (c == '.') {
+                if (!dot) {
+                    sb.append(c);
+                    dot = true;
+                }
+            } else {
+                sb.append(c);
+                dot = false;
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private String step4(String new_id) {
+        if (new_id.length() == 0) return new_id;
+        if (new_id.charAt(0) == '.') new_id = new_id.substring(1);
+        if (new_id.length() == 0) return new_id;
+        if (new_id.charAt(new_id.length() - 1) == '.') new_id = new_id.substring(0, new_id.length() - 1);
+        return new_id;
+    }
+
+    private String step5(String new_id) {
+        return new_id.length() == 0 ? "a" : new_id;
+    }
+
+    private String step6(String new_id) {
+        return new_id.length() > 15 ? step4(new_id.substring(0, 15)) : new_id;
+    }
+
+    private String step7(String new_id) {
+        while (new_id.length() < 3) {
+            new_id += new_id.charAt(new_id.length() - 1);
+        }
+        return new_id;
+    }
+
+    private boolean isValidChar(char c) {
+        return isAlp(c) || isNum(c) || isETC(c);
+    }
+    private boolean isAlp(char c) {
+        return c >= 'a' && c <= 'z';
+    }
+    private boolean isNum(char c) {
+        return c >= '0' && c <= '9';
+    }
+    private boolean isETC(char c) {
+        return c == '.' || c == '-' || c == '_';
+    }
+}
