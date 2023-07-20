dcaad25860f445314ff2df3d60d571b37b04b581
1000zoo <cjswldn99@gmail.com>
2023-07-20 09:56:35 +0900

    programmers / level2 / 탐욕법 / 큰 수 만들기

---
+++ src/codingtest_practice/level2/MakeBigNumber.java
@@ -0,0 +1,30 @@
+//2023-07-20
+//https://school.programmers.co.kr/learn/courses/30/lessons/42883
+//큰 수 만들기
+
+import java.util.*;
+
+class MakeBigNumber {
+    public String solution(String number, int k) {
+        Stack<Integer> stack = new Stack<>();
+        int[] arr = Arrays.stream(number.split("")).mapToInt(i -> Integer.parseInt(i)).toArray();
+        
+        for (int n : arr) {
+            while (!stack.isEmpty() && stack.peek() < n && k-- > 0) {
+                stack.pop();
+            }
+            stack.push(n);
+        }
+        
+        while (k > 0) {
+            stack.pop();
+            k--;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int a : stack) {
+            sb.append(a);
+        }
+        return sb.toString();
+    }
+}
