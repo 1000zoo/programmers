3705dc40ad3671f0aad202ba8ef051c6f7cb6693
1000zoo <cjswldn99@gmail.com>
2023-08-02 00:56:51 +0900

    programmers / level1 / 2021 카카오 채용연계형 인턴십 / 숫자 문자열과 영단어

--- src/codingtest_practice/level1/MixedNumberString.java
+++ src/codingtest_practice/level1/MixedNumberString.java
@@ -6,19 +6,17 @@ package codingtest_practice.level1;
 
 public class MixedNumberString {
     public int solution(String s) {
-        String[] list = {"zero", "one", "two", "three", "four",	"five",	"six", "seven", "eight", "nine"};
+        public int solution(String s) {
+            String[] list = {"zero", "one", "two", "three", "four",	"five",	"six", "seven", "eight", "nine"};
 
-        for (int i = 0; i < list.length; i++) {
-            s = replace(s, list[i], "" + i);
+            for (int i = 0; i < list.length; i++) {
+                s = s.replace(list[i], "" + i);
+            }
+            return Integer.parseInt(s);
         }
-        return Integer.parseInt(s);
-    }
 
-    private String replace(String ori, String from, String to) {
-        return ori.replace(from, to);
-    }
 
-    public int solution2(String s) {
+        public int solution2(String s) {
         int answer = 0;
         String temp = "";
 
