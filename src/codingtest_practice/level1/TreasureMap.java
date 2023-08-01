d1f405bcf3fa8430427f978ac0c2f6df83fd125e
1000zoo <cjswldn99@gmail.com>
2023-08-02 01:11:02 +0900

    programmers / 2018 KAKAO BLIND RECRUITMENT / [1차] 보물지도 / add new solution

--- src/codingtest_practice/level1/TreasureMap.java
+++ src/codingtest_practice/level1/TreasureMap.java
@@ -1,10 +1,22 @@
+//2022-10-11 (resolve: 2023-08-02)
 //https://school.programmers.co.kr/learn/courses/30/lessons/17681
-//2022-10-11
 //비밀지도
 
 package codingtest_practice.level1;
 
 public class TreasureMap {
+    public String[] newSolution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            String temp = Integer.toBinaryString(arr1[i] | arr2[i]);
+            temp = "0".repeat(n - temp.length()) + temp;
+            answer[i] = temp.replaceAll("0", " ").replaceAll("1", "#");
+        }
+
+        return answer;
+    }
+
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         for (int i = 0; i < n; i++) {
