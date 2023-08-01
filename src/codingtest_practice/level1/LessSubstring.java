ae1ec266bd1c9bb6afe2ab07d20698c8465c8bff
1000zoo <cjswldn99@gmail.com>
2023-08-02 02:25:34 +0900

    programmers / level1 / 연습문제 / 크기가 작은 부분 문자열/ 새로운 풀이

--- src/codingtest_practice/level1/LessSubstring.java
+++ src/codingtest_practice/level1/LessSubstring.java
@@ -1,10 +1,19 @@
-//
-//2022-12-29
+//2022-12-29 (resolve: 2023-08-02)
+//https://school.programmers.co.kr/learn/courses/30/lessons/147355?language=java
 //크기가 작은 부분 문자열
 
 package codingtest_practice.level1;
 
+import java.util.stream.*;
+
 public class LessSubstring {
+
+    public int newSolution(String t, String p) {
+        return (int) IntStream.range(0, t.length() - p.length() + 1).filter(
+                (i) -> Long.parseLong(t.substring(i, i + p.length())) <= Long.parseLong(p)
+        ).count();
+    }
+
     public int solution(String t, String p) {
         int answer = 0;
 
