351fb9a83fb49c848d1aeb8f7d87ae05b15a6d56
1000zoo <cjswldn99@gmail.com>
2023-08-04 13:53:52 +0900

    programmers / level1 / 연습문제 / 문자열 내 마음대로 정렬하기

--- src/codingtest_practice/level1/StringSortByNthIndex.java
+++ src/codingtest_practice/level1/StringSortByNthIndex.java
@@ -1,10 +1,25 @@
+//2022-10-17 (resolve: 2023-08-04)
 //https://school.programmers.co.kr/learn/courses/30/lessons/12915
-//2022-10-17
 //문자열 내 마음대로 정렬하기
 
 package codingtest_practice.level1;
 
+import java.util.*;
+
 public class StringSortByNthIndex {
+
+    public String[] newSolution(String[] strings, int n) {
+        Arrays.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.charAt(n) != o2.charAt(n) ?
+                        Character.compare(o1.charAt(n), o2.charAt(n)) :
+                        o1.compareTo(o2);
+            }
+        });
+        return strings;
+    }
+
     public String[] solution(String[] strings, int n) {
         for (int i = 0; i < strings.length - 1; i++) {
 
