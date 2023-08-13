a709bf2fb5f1d39e820c1c27893cc624445b1540
1000zoo <cjswldn99@gmail.com>
2023-08-13 23:50:42 +0900

    programmers / level1 / 연습문제 / 가장 가까운 같은 글자 / 새로운 풀이 추가

--- src/codingtest_practice/level1/NearestCharacter.java
+++ src/codingtest_practice/level1/NearestCharacter.java
@@ -1,12 +1,24 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/142086
-//2022-12-30
+//2022-12-30 (resolved: 2023-08-13)
 //가장 가까운 같은 글자
 
 package codingtest_practice.level1;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
 public class NearestCharacter {
+
+    public int[] newSolution(String s) {
+        return IntStream.range(0, s.length())
+            .map(i -> {
+                int index = s.substring(0, i).lastIndexOf(s.charAt(i));
+                return index == -1 ? -1 : i - index;
+            }
+                )
+            .toArray();
+    }
+
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
         Map<Character, Integer> map = new HashMap<>();
