03a75f82b2b2358a70183936d7d21ba265d68b3f
1000zoo <cjswldn99@gmail.com>
2023-09-01 14:06:20 +0900

    programmers / level3 / 2019 카카오 개발자 겨울 인턴십 / 징검다리 건너기

---
+++ src/codingtest_practice/level3/CrossSteppingStones.java
@@ -0,0 +1,24 @@
+//2023-09-01
+//https://school.programmers.co.kr/learn/courses/30/lessons/64062?language=java
+//징검다리 건너기
+
+package codingtest_practice.level3;
+
+import java.util.*;
+
+public class CrossSteppingStones {
+    public int solution(int[] stones, int k) {
+        int answer = Integer.MAX_VALUE;
+        Deque<int[]> dq = new ArrayDeque<>();
+
+        for (int i = 0; i < stones.length; i++) {
+            while (!dq.isEmpty() && dq.peekLast()[1] < stones[i]) dq.pop();
+            dq.addLast(new int[] {i, stones[i]});
+
+            if (i - dq.getFirst()[0] >= k) dq.removeFirst();
+            if (i >= k - 1) answer = Math.min(answer, dq.getFirst()[1]);
+        }
+
+        return answer;
+    }
+}
