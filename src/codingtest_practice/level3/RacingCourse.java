a23422e8508f9363017ae9037c650a55cd1cb64a
1000zoo <cjswldn99@gmail.com>
2023-09-23 23:01:33 +0900

    programmers / level3 / 2020 카카오 인턴십 / 경주로 건설

--- src/codingtest_practice/level3/RacingCourse.java
+++ src/codingtest_practice/level3/RacingCourse.java
@@ -3,35 +3,65 @@ package codingtest_practice.level3;
 public class RacingCourse {
 
     private int[][] map;
+    private int[][] dp;
+    private int[][] dirs;
 
     public int solution(int[][] board) {
         map = board;
-        boolean[][] visited = new boolean[map.length][map[0].length];
-        return dfs(new int[] {0, 0}, new int[] {1, 1}, Integer.MAX_VALUE, 0, visited);
+
+        dp = new int[board.length][board[0].length];
+        dirs = new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        dfs(new int[] {0, 0}, new int[] {1, 1}, 0, 0);
+        int d1 = dp[board.length - 1][board[0].length - 1];
+
+
+        dp = new int[board.length][board[0].length];
+        dirs = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        dfs(new int[] {0, 0}, new int[] {1, 1}, 0, 0);
+        int d2 = dp[board.length - 1][board[0].length - 1];
+
+
+        return Math.min(d1, d2);
     }
 
-    private int dfs(int[] currPos, int[] dir, int min, int cost, boolean[][] visited) {
-        if (currPos[0] == map.length - 1 &&
-                currPos[1] == map[0].length - 1) return Math.min(min, cost + 100);
-
-        int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private void dfs(int[] currPos, int[] dir, int straight, int corner) {
+        if (currPos[0] == map.length - 1 && currPos[1] == map[0].length - 1) {
+            updateDP(currPos, straight, corner);
+            return;
+        }
         int y = currPos[0];
         int x = currPos[1];
-        visited[y][x] = true;
 
         for (int[] d : dirs) {
             int[] temp = {y + d[0], x + d[1]};
             if (!boundary(temp)) continue;
             if (map[temp[0]][temp[1]] == 1) continue;
-            if (visited[temp[0]][temp[1]]) continue;
 
-            min = Math.min(min, dfs(temp, d, min, cost + getPrice(dir, d), visited.clone()));
+            int s = straight + 1;
+            int c = corner;
+
+            if (isCorner(dir, d)) c += 1;
+            if (updateDP(temp, s, c)) dfs(temp, d, s, c);
+
         }
-        return min;
     }
 
-    private int getPrice(int[] d1, int[] d2) {
-        return isCorner(d1, d2) ? 500 : 100;
+    private boolean updateDP(int[] curr, int straight, int corner) {
+        int cost = getCost(straight, corner);
+
+        if (dp[curr[0]][curr[1]] == 0) {
+            dp[curr[0]][curr[1]] = cost;
+            return true;
+        }
+        if (dp[curr[0]][curr[1]] >= cost) {
+            dp[curr[0]][curr[1]] = cost;
+            return true;
+        }
+        return false;
+    }
+
+    private int getCost(int straight, int corner) {
+        return 100 * straight + 500 * corner;
     }
 
     private boolean boundary(int[] pos) {
