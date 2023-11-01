bd98f30f2b37532358aad0ddc881fe4368690e72
1000zoo <cjswldn99@gmail.com>
2023-11-01 19:51:28 +0900

    programmers / level3 / 2022 KAKAO TECH INTERNSHIP / 등산코스 정하기

--- src/codingtest_practice/level3/MountainPathFinder.java
+++ src/codingtest_practice/level3/MountainPathFinder.java
@@ -7,79 +7,66 @@ package codingtest_practice.level3;
 import java.util.*;
 
 public class MountainPathFinder {
-    Map<Integer, List<List<Integer>>> graph;
-    Set<Integer> gateSet;
-    Set<Integer> summitSet;
-    Map<Integer, Integer> minIntensityMap;
+    Map<Integer, List<int[]>> graph = new HashMap<>();
+    Set<Integer> gateSet = new HashSet<>();
+    Set<Integer> summitSet = new HashSet<>();
+    int top = -1;
 
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
-        graph = new HashMap<>();
-        gateSet = new HashSet<>();
-        summitSet = new HashSet<>();
-        minIntensityMap = new HashMap<>();
-
-        initMap(paths);
-        initSet(gateSet, gates);
-        initSet(summitSet, summits);
-
-        int[] answer = {-1, Integer.MAX_VALUE};
-
-        for (int gate : gates) {
-            dfs(gate, -1, answer, new HashSet<>(Arrays.asList(gate)));
-        }
-
-        return answer;
-    }
-
-    private void dfs(int curr, int intensity, int[] answer, Set<Integer> visited) {
-        if (summitSet.contains(curr)) {
-            updateAnswer(curr, intensity, answer);
-        }
-
-        List<List<Integer>> nextNodes = graph.get(curr);
-
-        for (List<Integer> node : nextNodes) {
-            int n = node.get(0);
-            int w = node.get(1);
-
-            if (visited.contains(n) || gateSet.contains(n)) continue;
-
-            int newIntensity = Math.max(intensity, w);
-
-            if (minIntensityMap.containsKey(n) && minIntensityMap.get(n) <= newIntensity) {
-                continue;
-            }
-
-            minIntensityMap.put(n, newIntensity);
-            Set<Integer> newVisited = new HashSet<>(visited);
-            newVisited.add(n);
-            dfs(n, newIntensity, answer, newVisited);
-        }
-    }
-
-    private void updateAnswer(int summit, int intensity, int[] answer) {
-        if (answer[1] > intensity) {
-            answer[0] = summit;
-            answer[1] = intensity;
-        } else if (answer[1] == intensity) {
-            answer[0] = Math.min(summit, answer[0]);
-        }
-    }
-
-    private void initMap(int[][] paths) {
         for (int[] path : paths) {
-            int n1 = path[0];
-            int n2 = path[1];
-            int w = path[2];
-
-            graph.computeIfAbsent(n1, k -> new ArrayList<>()).add(List.of(n2, w));
-            graph.computeIfAbsent(n2, k -> new ArrayList<>()).add(List.of(n1, w));
+            graph.computeIfAbsent(path[0], k -> new ArrayList<>()).add(new int[]{path[1], path[2]});
+            graph.computeIfAbsent(path[1], k -> new ArrayList<>()).add(new int[]{path[0], path[2]});
         }
+        for (int gate : gates) {
+            gateSet.add(gate);
+        }
+        for (int summit : summits) {
+            summitSet.add(summit);
+        }
+
+        int l = 1;
+        int r = 10000000;
+
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (bfs(m)) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return new int[]{top, l};
     }
 
-    private void initSet(Set<Integer> set, int[] array) {
-        for (int arr : array) {
-            set.add(arr);
+    private boolean bfs(int cut) {
+        Queue<Integer> queue = new LinkedList<>();
+        Map<Integer, Integer> visited = new HashMap<>();
+
+        for (int gate : gateSet) {
+            queue.add(gate);
+            visited.put(gate, 1);
         }
+
+        int last = Integer.MAX_VALUE;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            for (int[] next : graph.getOrDefault(current, new ArrayList<>())) {
+                if (next[1] > cut || visited.containsKey(next[0]) || gateSet.contains(next[0])) continue;
+                visited.put(next[0], 1);
+                if (summitSet.contains(next[0])) {
+                    last = Math.min(last, next[0]);
+                    continue;
+                }
+                queue.add(next[0]);
+            }
+        }
+
+        if (last != Integer.MAX_VALUE) {
+            top = last;
+            return true;
+        }
+        return false;
     }
 }
