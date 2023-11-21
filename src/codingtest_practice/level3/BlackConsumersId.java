ba02a7db49929f13efc988c8ab30ae841f5bf52b
1000zoo <cjswldn99@gmail.com>
2023-11-22 07:09:44 +0900

    programmers / level3 / 2019 카카오 개발자 겨울 인턴십 / 불량사용자 / 재풀이

--- src/codingtest_practice/level3/BlackConsumersId.java
+++ src/codingtest_practice/level3/BlackConsumersId.java
@@ -1,4 +1,4 @@
-//2023-08-20
+//2023-08-20 (새로운 풀이: 2023-11-22)
 //https://school.programmers.co.kr/learn/courses/30/lessons/64064?language=java
 //불량 사용자
 
@@ -7,49 +7,67 @@ package codingtest_practice.level3;
 import java.util.*;
 
 public class BlackConsumersId {
-    private Map<Integer, List<String>> map;
-    private Set<String> set;
+
+    private final Set<String> checkDuplicate = new HashSet<>();
 
     public int solution(String[] userId, String[] bannedId) {
-        int answer = 0;
-        map = new HashMap<>();
-        set = new HashSet<>();
+        List<List<String>> candidateList = new ArrayList<>();
 
-        for (int i = 0; i < bannedId.length; i++) {
-            List<String> temp = new ArrayList<>();
-            for (String user : userId) {
-                if (isSame(user, bannedId[i])) temp.add(user);
-            }
-            map.put(i, temp);
+        for (String id : bannedId) {
+            candidateList.add(getCandidateListOf(id, userId));
         }
-
-        dfs(0, new ArrayList<>());
-        return set.size();
+        recursion(candidateList, new HashSet<>(), 0);
+        return checkDuplicate.size();
     }
 
-    private void dfs(int index, ArrayList<String> selected) {
-        if (index == map.size()) {
-            ArrayList<String> temp = (ArrayList<String>)selected.clone();
-            Collections.sort(temp);
-            set.add(temp.toString());
+    private void recursion(List<List<String>> list, Set<String> set, int index) {
+        if (list.size() == index) {
+            addSetToOrderedString(set);
             return;
         }
 
-        for (String p : map.get(index)) {
-            if (selected.contains(p)) continue;
-            selected.add(p);
-            dfs(index + 1, selected);
-            selected.remove(selected.size() - 1);
+        List<String> currList = list.get(index);
+
+        for (String id : currList) {
+            if (set.contains(id)) {
+                continue;
+            }
+            set.add(id);
+            recursion(list, set, index + 1);
+            set.remove(id);
         }
     }
 
-    private boolean isSame(String u, String b) {
-        if (u.length() != b.length()) return false;
+    private void addSetToOrderedString(Set<String> set) {
+        List<String> list = new ArrayList<>(set);
+        Collections.sort(list);
+        checkDuplicate.add(list.toString());
+    }
 
-        for (int i = 0; i < u.length(); i++) {
-            if (u.charAt(i) == b.charAt(i) || b.charAt(i) == '*') continue;
+    private List<String> getCandidateListOf(String bannedId, String[] userId) {
+        List<String> list = new ArrayList<>();
+
+        for (String id : userId) {
+            if (isPossible(id, bannedId)) {
+                list.add(id);
+            }
+        }
+
+        return list;
+    }
+
+    private boolean isPossible(String userId, String bannedId) {
+        if (userId.length() != bannedId.length()) {
             return false;
         }
+
+        for (int i = 0; i < userId.length(); i++) {
+            if (bannedId.charAt(i) == '*' || bannedId.charAt(i) == userId.charAt(i)) {
+                continue;
+            }
+            return false;
+        }
+
         return true;
     }
 }
