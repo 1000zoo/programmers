f60b8df0e4d8410c0864d500ad44b2fd131edbec
1000zoo <cjswldn99@gmail.com>
2023-11-22 11:52:44 +0900

    programmers / level3 / 2019 카카오 개발자 겨울 인턴십 / 징검다리 건너기 / 새로운 풀이 추가

--- src/codingtest_practice/level3/CrossSteppingStones.java
+++ src/codingtest_practice/level3/CrossSteppingStones.java
@@ -1,4 +1,4 @@
-//2023-09-01
+//2023-09-01 (새로운 풀이: 2023-11-22)
 //https://school.programmers.co.kr/learn/courses/30/lessons/64062?language=java
 //징검다리 건너기
 
@@ -7,7 +7,42 @@ package codingtest_practice.level3;
 import java.util.*;
 
 public class CrossSteppingStones {
+
+    // 이진탐색
     public int solution(int[] stones, int k) {
+        int left = 0;
+        int right = Integer.MAX_VALUE;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (isValid(stones, mid, k)) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return right;
+    }
+
+    private boolean isValid(int[] origin, int mid, int k) {
+        int count = 0;
+        int[] stones = origin.clone();
+        for (int i = 0; i < stones.length; i++) {
+            stones[i] -= mid;
+            if (stones[i] < 0) count++;
+            else count = 0;
+            if (count >= k) return false;
+        }
+
+        return true;
+    }
+
+    // ========================================================
+
+    // Sliding Window 적용 풀이
+    public int oldSolution(int[] stones, int k) {
         int answer = Integer.MAX_VALUE;
         Deque<int[]> dq = new LinkedList<>();
 
