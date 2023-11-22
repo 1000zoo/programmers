e1fda6a23fa90a24316b3cb28a1b64082442a9f2
1000zoo <cjswldn99@gmail.com>
2023-11-22 09:12:57 +0900

    programmers / level3 / 2020 카카오 인턴십 / 보석쇼핑

--- src/codingtest_practice/level3/JewelryShopping.java
+++ src/codingtest_practice/level3/JewelryShopping.java
@@ -1,4 +1,4 @@
-//2023-08-20
+//2023-08-20 (새로운 풀이: 2023-11-22)
 //https://school.programmers.co.kr/learn/courses/30/lessons/67258
 //보석 쇼
 
@@ -8,49 +8,37 @@ import java.util.*;
 
 public class JewelryShopping {
 
-    private Map<String, Integer> gemCounts;
-
     public int[] solution(String[] gems) {
-        int[] answer = {0, gems.length};
-        String[] g = new String[gems.length + 1];
-        Set<String> types = new HashSet<>();
-        gemCounts = new HashMap<>();
+        int[] answer = new int[] {0, gems.length};
+        Map<String, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < gems.length; i++) {
-            types.add(gems[i]);
-            g[i + 1] = gems[i];
-        }
+        Set<String> set = new HashSet<>(Arrays.asList(gems));
 
-        int total = types.size();
-        System.out.println(total);
-        int s = 1;
-        int e = 0;
-        boolean sTurn = false;
+        int left = 0;
+        int right = -1;
+        boolean leftTurn = false;
 
-        while ((s != e) || (s != g.length)) {
-            if (sTurn) rmGem(g[s++]);
-            else addGem(g[++e]);
+        while (true) {
+            if (leftTurn) {
+                String leftGem = gems[left++];
+                map.put(leftGem, map.get(leftGem) - 1);
+                if (map.get(leftGem) == 0) {
+                    map.remove(leftGem);
+                }
+            } else {
+                String rightGem = gems[++right];
+                map.put(rightGem, map.getOrDefault(rightGem, 0) + 1);
+            }
 
-            if (gemCounts.size() < total) {
-                sTurn = false;
-                if (e == g.length - 1) return answer;
-            } else if (gemCounts.size() == total) {
-                sTurn = true;
-                if (answer[1] - answer[0] > e - s) {
-                    answer = new int[]{s, e};
+            if (map.size() < set.size()) {
+                leftTurn = false;
+                if (right == gems.length - 1) return answer;
+            } else if (map.size() == set.size()) {
+                leftTurn = true;
+                if (answer[1] - answer[0] > right - left) {
+                    answer = new int[] {left + 1, right + 1};
                 }
             }
         }
-
-        return answer;
-    }
-
-    private void rmGem(String gem) {
-        gemCounts.put(gem, gemCounts.get(gem) - 1);
-        if (gemCounts.get(gem) == 0) gemCounts.remove(gem);
-    }
-
-    private void addGem(String gem) {
-        gemCounts.put(gem, gemCounts.getOrDefault(gem, 0) + 1);
     }
 }
